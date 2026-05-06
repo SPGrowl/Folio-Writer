6f023e1bf383e78c3b045710d29eb0bb39898964
SPGrowl“ <3422568995@qq.com>
2026-05-06 22:26:34 +0800

    fix wrong git commit 5.6

---
+++ src/router/index.ts
@@ -0,0 +1,54 @@
+import type { App } from 'vue'
+import type { RouteRecordRaw } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
+import { setupPageGuard } from './permission'
+import { ChatLayout } from '@/views/chat/layout'
+
+const routes: RouteRecordRaw[] = [
+  {
+    path: '/',
+    name: 'Root',
+    component: ChatLayout,
+    redirect: '/chat',
+    children: [
+      {
+        // 匹配聊天页面，路径为/chat/:uuid?，其中:uuid是可选参数
+        path: '/chat/:uuid?',
+        name: 'Chat',
+        component: () => import('@/views/chat/index.vue'),
+      },
+    ],
+  },
+
+  {
+    path: '/404',
+    name: '404',
+    component: () => import('@/views/exception/404/index.vue'),
+  },
+
+  {
+    path: '/500',
+    name: '500',
+    component: () => import('@/views/exception/500/index.vue'),
+  },
+
+  {
+    // 匹配所有路径，如果路径不存在，则重定向到404页面
+    path: '/:pathMatch(.*)*',
+    name: 'notFound',
+    redirect: '/404',
+  },
+]
+
+export const router = createRouter({
+  history: createWebHashHistory(),
+  routes,
+  scrollBehavior: () => ({ left: 0, top: 0 }),
+})
+
+setupPageGuard(router)
+
+export async function setupRouter(app: App) {
+  app.use(router)
+  await router.isReady()
+}
